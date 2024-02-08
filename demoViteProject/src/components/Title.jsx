52c91225deb1891e4196b8b7fd378925b69923a6
AbhiPrajapati <prajapatiabhishek1111@gmail.com>
2024-02-08 11:10:48 +0530

    Vite Project Added

---
+++ demoViteProject/src/components/Title.jsx
@@ -0,0 +1,11 @@
+function Title()
+{
+    return(
+        <div className="name-header">
+            <p className="name-header-text">ABHISHEK D. PRAJAPATI</p>
+        </div>
+    );
+}
+
+
+export default Title;
---
+++ demoViteProject/src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import Title from './components/Title.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Title></Title>
+  </React.StrictMode>,
+)
